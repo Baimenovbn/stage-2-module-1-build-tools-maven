a133f645008816be74bda0d496ce2791913311c5
Baimenovbn <baibn@mail.ru>
2023-01-31 12:55:44 +0600

    feat: finish task

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str.length() > 0 && str.charAt(0) == '0') {
+            return false;
+        }
+
+        try {
+            return Double.parseDouble(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
